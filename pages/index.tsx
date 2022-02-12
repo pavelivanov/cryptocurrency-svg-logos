507c1bfb86fc9f861aaa31e9109db138780e3392
Pavel Ivanov <grammka@gmail.com>
2022-02-12 20:42:39 +0300

    update logos

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import type { NextPage } from 'next'
+
+import s from './Home.module.css'
+
+import logos from './logos'
+
+
+const Home: NextPage = () => {
+  const [ query, setQuery ] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }
+
+  const filteredLogos = !query ? logos : logos.filter((name) => name.includes(query.toLowerCase()))
+
+  return (
+    <div className={s.page}>
+      <div className={s.container}>
+        <input className={s.input} onChange={handleChange} />
+        <div className={s.items}>
+          {
+            filteredLogos.map((name) => (
+              <a key={name} className={s.item} href={`/images/logos/${name}.svg`} target="_blank">
+                <img className={s[name]} src={`/images/logos/${name}.svg`} alt={name} />
+              </a>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Home
---
+++ scripts/logos-loader.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+
+
+const folder = path.resolve('public/images/logos')
+const pattern = path.resolve(folder, '**/*.svg')
+const outputPath = path.resolve('pages/logos.ts')
+
+const generate = () => {
+  const filePaths = glob.sync(pattern)
+  const fileNames = filePaths.map((filePath) => filePath.replace(/.+images\/logos\//, '').replace('.svg', ''))
+  const content = `export default [ '${fileNames.join("', '")}' ]`
+
+  fs.writeFileSync(outputPath, content)
+}
+
+generate()
